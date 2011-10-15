e2334b734c02f9b422028c6e7996e44181827913
Egor Alekseyev <egor.alekseyev@gmail.com>
2011-10-15 20:29:36 +0400

    Added zoom detection functionality

--- content_script_on_document_start.js
+++ content_script_on_document_start.js
@@ -1,49 +1,11 @@
-function beforeLoadHandler(event)
-{   
-//    alert(event.target.nodeName);
-//    event.preventDefault();
-}
 
-document.addEventListener("beforeload", beforeLoadHandler, true);
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse) {
+     var clientWidth = document.documentElement.clientWidth;
+     var zoom_level = document.width/clientWidth;
+     if (zoom_level > 3) {
+	zoom_level=3;
+     }
+      sendResponse({zoom_level:zoom_level});
+  });
 
-setTimeout(function() {
-//    alert(document.getElementsByTagName("img").length);
-}, 210);
-/*
- *
- *      "matches": ["http://*.wikipedia.org/*"],
-
-document.addEventListener("beforeload", beforeLoadHandler, false);
-*/
-/*
-while (document.getElementsByTagName("img").length > 0) {
-	var element = document.getElementsByTagName("img")[0];
-        
-	var parentElement = element.parentNode;
-	parentElement.removeChild(element);
-
-}
-*/
-/*
-var thumbnails = document.getElementsByTagName("img");
-
-var zoom_level = 144;
-
-function setup_event_listener(image_element, url) {
-	var result = thumbnail.addEventListener("error", function() {
-		image_element.src = url;
-	},false);		 	
-}
-
-for (var i = 0; i < thumbnails.length;i++) {
-	var thumbnail = thumbnails[i];
-	var matches = thumbnail.src.match("/[0-9]+px-");
-	if (matches != null) {
-		var resolution = Number(matches[0].match("[0-9]+"));
-		var new_resolution = Math.round(resolution*zoom_level/100);		
-	}
-	setup_event_listener(thumbnail, thumbnail.src);
-	thumbnail.src = thumbnail.src.replace(new RegExp("/[0-9]+px-"), "/" + new_resolution + "px-");
-}
-
-*/
